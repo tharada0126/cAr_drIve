babb2e269afd2305ee288281acb0389256f0a52e
Tomohiro Harada <tomohiro.harada0126@gmail.com>
2020-10-09 12:27:27 +0900

    multiple cars

--- Assets/Scripts/CarAgent.cs
+++ Assets/Scripts/CarAgent.cs
@@ -8,24 +8,62 @@ public class CarAgent : Agent
 {
     public float speed = 10f;
     public float torque = 10f;
+    private float prevVertical = 0f;
+    private float prevHorizontal = 0f;
 
     public int score = 0;
     public bool resetOnCollision = true;
 
+    public int id = 0;
+    private int new_id = 0;
+
     private Transform _track;
 
+    public int time = 0;
+    public bool generateNew = true;
+
+    private Vector3 _initPosition;
+    private Quaternion _initRotation;
+    private int _notMoveCount = 0;
     public override void Initialize()
     {
         GetTrackIncrement();
+        _initPosition = transform.localPosition;
+        _initRotation = transform.localRotation;
+        time = 0;
+        new_id = 0;
+    }
+
+    void Update()
+    {
+        if(!generateNew || id != 0) return;
+        //Debug.Log(time);
+        if(time > 500){
+            //Debug.Log("add new car");
+            var gameObject = Instantiate(this, _initPosition, _initRotation);
+            new_id++;
+            gameObject.id = new_id;
+            gameObject.transform.parent = this.transform.parent.gameObject.transform;
+            gameObject.transform.localPosition = _initPosition;
+            gameObject.transform.localRotation = _initRotation;
+            gameObject.speed = Random.Range(5, 16);
+            //Debug.Log(gameObject.transform);
+            time = 0;
+        }
     }
 
     private void MoveCar(float horizontal, float vertical, float dt)
     {
+        if(generateNew){
+            time++;
+        }
         float distance = speed * vertical;
         transform.Translate(distance * dt * Vector3.forward);
 
         float rotation = horizontal * torque * 90f;
         transform.Rotate(0f, rotation * dt, 0f);
+        prevHorizontal = horizontal;
+        prevVertical = vertical;
     }
 
     public override void OnActionReceived(float[] vectorAction)
@@ -39,6 +77,16 @@ public class CarAgent : Agent
         int reward = GetTrackIncrement();
         
         var moveVec = transform.position - lastPos;
+        /*
+        if(Vector3.Distance(lastPos, transform.position) < 0.05f){
+            _notMoveCount += 1;
+            if(_notMoveCount > 10){
+                _notMoveCount = 0;
+                SetReward(-1f);
+                EndEpisode();
+            }
+        }
+        //*/
         float angle = Vector3.Angle(moveVec, _track.forward);
         float bonus = (1f - angle / 90f) * Mathf.Clamp01(vertical) * Time.fixedDeltaTime;
         AddReward(bonus + reward);
@@ -59,14 +107,174 @@ public class CarAgent : Agent
         float angle = Vector3.SignedAngle(_track.forward, transform.forward, Vector3.up);
 
         vectorSensor.AddObservation(angle / 180f);
-        vectorSensor.AddObservation(ObserveRay(1.5f, .5f, 25f));
-        vectorSensor.AddObservation(ObserveRay(1.5f, 0f, 0f));
-        vectorSensor.AddObservation(ObserveRay(1.5f, -.5f, -25f));
-        vectorSensor.AddObservation(ObserveRay(-1.5f, 0, 180f));
+        //float speed, torque;
+        string tag;
+        //Quaternion rotation;
+        Vector3 diff;
+        //vectorSensor.AddObservation(ObserveRay(1.5f, .5f, 25f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(1.5f, .5f, 25f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(1.5f, 0f, 0f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(1.5f, 0f, 0f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(1.5f, -.5f, -25f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(1.5f, -.5f, -25f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(-1.5f, .5f, 155f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(-1.5f, .5f, 155f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(-1.5f, 0, 180f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(-1.5f, 0f, 180f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(-1.5f, -.5f, -155f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(-1.5f, -.5f, -155f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(0f, .5f, 90f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(0f, .5f, 90f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        //vectorSensor.AddObservation(ObserveRay(0f, -.5f, -90f, out speed, out torque, out rotation, out tag));
+        vectorSensor.AddObservation(ObserveRay(0f, -.5f, -90f, out diff, out tag));
+        //vectorSensor.AddObservation((180.0f + Quaternion.Angle(rotation, this.transform.rotation)) / 360.0f);
+        //vectorSensor.AddObservation(speed);
+        //vectorSensor.AddObservation(torque);
+        vectorSensor.AddObservation(diff.x);
+        vectorSensor.AddObservation(diff.y);
+        if(tag == "car"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        if(tag == "wall"){
+            vectorSensor.AddObservation(1);
+        }
+        else{
+            vectorSensor.AddObservation(0);
+        }
+        vectorSensor.AddObservation(this.speed);
+        vectorSensor.AddObservation(this.torque);
     }
 
-    private float ObserveRay(float z, float x, float angle)
+    //private float ObserveRay(float z, float x, float angle, out float speed, out float torque, out Quaternion rotation, out string tag)
+    private float ObserveRay(float z, float x, float angle, out Vector3 diff, out string tag)
     {
+        //speed = 0.0f;
+        //torque = 0.0f;
+        //rotation = Quaternion.identity;
+        diff = Vector3.zero;
+        tag = "none";
         var tf = transform;
     
         // Get the start position of the ray
@@ -77,9 +285,23 @@ public class CarAgent : Agent
         // Get the angle of the ray
         var eulerAngle = Quaternion.Euler(0, angle, 0f);
         var dir = eulerAngle * tf.forward;
+        RaycastHit hit;
     
         // See if there is a hit in the given direction
-        Physics.Raycast(position, dir, out var hit, RAY_DIST);
+        var rayhit = Physics.Raycast(position, dir, out hit, RAY_DIST);
+        if(rayhit){
+            tag = hit.collider.tag;
+            if(hit.collider.tag == "car"){
+                CarAgent agent = hit.collider.gameObject.GetComponent(typeof(CarAgent)) as CarAgent;
+                var self_dir = Quaternion.Euler(0, this.torque * this.prevHorizontal * 90f, 0) * (this.transform.forward * this.prevVertical * this.speed);
+                var agent_dir = Quaternion.Euler(0, agent.torque * agent.prevHorizontal * 90f, 0) * (agent.transform.forward * agent.prevVertical * agent.speed);
+                diff = agent_dir - self_dir;
+
+                //speed = agent.prevVertical * agent.speed / this.speed;
+                //torque = agent.prevHorizontal * agent.torque / this.torque;
+                //rotation = agent.transform.rotation;
+            }
+        }
         return hit.distance >= 0 ? hit.distance / RAY_DIST : -1f;
     }
 
@@ -109,17 +331,29 @@ public class CarAgent : Agent
     {
         if (resetOnCollision)
         {
-            transform.localPosition = Vector3.zero;
-            transform.localRotation = Quaternion.identity;
+            //transform.localPosition = Vector3.zero;
+            //transform.localPosition = new Vector3(0, 0, 5 - id * 7);
+            transform.localPosition = _initPosition;
+            transform.localRotation = _initRotation;
+            //transform.localRotation = Quaternion.identity;
+            //time = 0;
         }
     }
 
     private void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("wall"))
+        if (other.gameObject.CompareTag("wall") || other.gameObject.CompareTag("car"))
         {
             SetReward(-1f);
-            Done();
+            EndEpisode();
+            if(other.gameObject.CompareTag("car"))
+            {
+                var otherAgent = (CarAgent)other.gameObject.GetComponent(typeof(CarAgent));
+                if(this.id < otherAgent.id)
+                {
+                    Destroy(other.gameObject);
+                }
+            }
         }
     }
 }
